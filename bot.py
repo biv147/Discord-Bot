b61b05669fdcebadba68a7bc8a6c2804ca186d59
biv147 <37488403+biv147@users.noreply.github.com>
2020-06-10 17:45:18 -0400

    added more responses for 8ball function

--- bot.py
+++ bot.py
@@ -23,8 +23,27 @@ async def ping(ctx): #returns a message into the chat
 
 @client.command(aliases = ['8ball'])
 async def _8ball(ctx, *, question): # answers a users question, also '*' allows many parameters as one parameter
-    responses = ['It is certain', 'Dont count on it']
+    responses = ["It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
     await ctx.send(f'Question: {question}\nAswer: {random.choice(responses)}')
 
 
-client.run('NzIwMzYwMTY1MDQ3NTMzNjA5.XuE9gA.eGvIPM-Ly1Ay3s8lJQFXPaK_qe4')
+client.run("token") #replace token with the bot token
